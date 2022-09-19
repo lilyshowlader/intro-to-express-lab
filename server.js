8576cfc6b1319a4b16778dbefb2c92cf041b2a40
lilyshowlader <lilyshowlader@gmail.com>
2022-09-19 19:27:24 -0400

    set up

---
+++ server.js
@@ -0,0 +1,32 @@
+// 1. import modules
+
+import express from 'express'
+
+// 2. Create Express app
+
+const app = express()
+
+// 3. Configure the app (app.set)
+
+
+
+// 4. Mount Middleware (app.use)
+
+
+
+// 5. Mount routes
+app.get('/', function(req, res) {
+  res.send('<h1>hello, kfriend</h1>')
+})
+// The first argument provided to app.get, /, defines the path for the route. In this case, the root of the application (the root of the application is just the hostname like localhost:3000)
+
+app.get('/home', function (req, res) {
+  res.send('<h1>home page</h1>')
+})
+
+
+// 6. Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
